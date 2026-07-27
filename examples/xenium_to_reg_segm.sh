b38ee573229af773f45d28dc351a78763c39cd01
Stanislaw Makarchuk <49611600+StasMakarchuk@users.noreply.github.com>
2026-07-27 15:31:54 +0100

    Update paths in xenium_to_reg_segm.sh script

--- examples/xenium_to_reg_segm.sh
+++ examples/xenium_to_reg_segm.sh
@@ -1,7 +1,7 @@
-python assign_cells.py --xenium_bundle_path "/lustre/scratch127/cellgen/cellgeni/public_datasets/Atera/WTA_Preview_FFPE_Breast_Cancer_outs/" \
-                        --image_path "/lustre/scratch127/cellgen/cellgeni/public_datasets/Atera//WTA_Preview_FFPE_Breast_Cancer_outs_supplemental_files/WTA_Preview_FFPE_Breast_Cancer_he_image.ome.tif" \
-                        --polygons_parquet_path "/lustre/scratch127/cellgen/cellgeni/tickets/REQ-70619/actions/stardist_he_human_breast/WTA_Preview_FFPE_Breast_Cancer_stardist_he_human_breast_polygons_fullcoords.parquet" \
-                        --transform_csv_path "/lustre/scratch127/cellgen/cellgeni/tickets/REQ-70619/actions/WTA_Preview_FFPE_Breast_Cancer_he_alignment.csv" \
+python assign_cells.py --xenium_bundle_path "/path/to/public_datasets/Atera/WTA_Preview_FFPE_Breast_Cancer_outs/" \
+                        --image_path "/path/to/public_datasets/Atera/WTA_Preview_FFPE_Breast_Cancer_outs_supplemental_files/WTA_Preview_FFPE_Breast_Cancer_he_image.ome.tif" \
+                        --polygons_parquet_path "/path/to/WTA_Preview_FFPE_Breast_Cancer_stardist_he_human_breast_polygons_fullcoords.parquet" \
+                        --transform_csv_path "/path/to/WTA_Preview_FFPE_Breast_Cancer_he_alignment.csv" \
                         --output_matches_csv "output_test/matched_cells_test1.csv" \
                         --he_coordinate_units "auto" \
                         --invert_matrix True \
